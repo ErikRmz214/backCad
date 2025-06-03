ebf9e5ae26f7186535c254c93196143cc7d3482b
ErikRmz <147409948+ErikRmz214@users.noreply.github.com>
2025-06-03 13:52:34 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,15 +2,14 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "caducadosdb",
-    password: "R@psodia2133",
-    port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // Necesario para conectar con Render
+  },
 });
 
 pool.connect()
-    .then(() => console.log("✅ Base de datos conectada"))
-    .catch(err => console.error("❌ Error de conexión a la BD:", err));
+  .then(() => console.log("✅ Base de datos conectada"))
+  .catch(err => console.error("❌ Error de conexión a la BD:", err));
 
 module.exports = pool;
